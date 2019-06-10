0687c632a96af03cadbcec7554e57ddcd5b3c0d0
Todd Williams <toddwil@microsoft.com>
2019-06-10 10:16:10 -0700

    Added grab scene content to summon item from an interactable for a closer inspection.

---
+++ Assets/ZoomReturn.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomReturn : MonoBehaviour
+{
+    /// <summary>
+    /// Add pause or safe area to manager
+    /// when in contact with manager, do something visual
+    /// also pause, don't let the original Interactable do anything if interacted with again
+    /// Make the placed object interactable so it can be picked up or clicked to return.
+    /// Should get picked up and put back down or clicked.
+    /// </summary>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ZoomToView.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomToView : MonoBehaviour
+{
+    public Transform ObjectToZoom;
+    public GameObject ZoomContainerPrefab;
+    public float ZoomSpeed = 3;
+
+    private GameObject zoomedObject;
+    private Transform objectParent;
+    private IMixedRealityPointer currentPointer;
+    private Vector3 cachedLocalPosition;
+    private Quaternion cachedLocalRotation;
+
+    private ZoomReturn objectManager;
+    private GameObject safeArea;
+
+    public void StartZoom(GameObject interactable)
+    {
+        if(zoomedObject == null)
+        {
+            zoomedObject = Instantiate(ZoomContainerPrefab);
+            Interactable button = interactable.GetComponent<Interactable>();
+            currentPointer = null;
+            foreach (IMixedRealityInputSource source in button.PressingInputSources)
+            {
+                for (int i = 0; i < source.Pointers.Length; i++)
+                {
+                    if (source.Pointers[i] is PokePointer)
+                    {
+                        currentPointer = source.Pointers[i];
+                        break;
+                    }
+                    
+                }
+            }
+
+            if(currentPointer != null)
+            {
+                zoomedObject.transform.position = currentPointer.Position;
+                zoomedObject.transform.rotation = currentPointer.Rotation;
+            }
+            
+            cachedLocalPosition = ObjectToZoom.transform.localPosition;
+            cachedLocalRotation = ObjectToZoom.transform.localRotation;
+            objectParent = ObjectToZoom.transform.parent;
+            ObjectToZoom.parent = zoomedObject.transform;
+        }
+    }
+
+    public void StopZoom()
+    {
+        if(zoomedObject != null)
+        {
+            ObjectToZoom.transform.parent = objectParent;
+            //ObjectToZoom.transform.localPosition = cachedLocalPosition;
+            ObjectToZoom.transform.localRotation = cachedLocalRotation;
+            Destroy(zoomedObject);
+            zoomedObject = null;
+        }
+    }
+
+    private void Update()
+    {
+        float decay = Mathf.Pow(10, -ZoomSpeed);
+        float lerpSpeed = 1 - Mathf.Pow(decay, Time.deltaTime);
+
+
+        if (zoomedObject != null && currentPointer != null)
+        {
+            zoomedObject.transform.position = currentPointer.Position;
+            zoomedObject.transform.rotation = currentPointer.Rotation;
+            
+
+            ObjectToZoom.transform.localPosition = Vector3.Lerp(ObjectToZoom.transform.localPosition, Vector3.zero, lerpSpeed);
+        }
+        else
+        {
+            ObjectToZoom.transform.localPosition = Vector3.Lerp(ObjectToZoom.transform.localPosition, cachedLocalPosition, lerpSpeed);
+        }
+    }
+}
